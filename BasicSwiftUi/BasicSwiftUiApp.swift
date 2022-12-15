0b10c48cc2bf122192078836849374aac6e2edfc
imran <imran40031@gmail.com>
2022-12-15 10:27:31 +0600

    Initial Commit

---
+++ BasicSwiftUi/BasicSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicSwiftUiApp.swift
+//  BasicSwiftUi
+//
+//  Created by md imran on 15/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BasicSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
